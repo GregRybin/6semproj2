19cbbfff3fc36b9ecb445e36c0bfc781e42d5305
Greg <45618727+GregRybin@users.noreply.github.com>
2026-04-02 00:10:36 +0300

    Update pre_main.cpp

--- pre_main.cpp
+++ pre_main.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 
+struct Point {
+    double x, y;
+}
+
+struct Line {
+    Point p, v;
+    
+    Line(const Point& p, const Point& v) : p(p), v(v) {}
+};
+
+struct Segment {
+    Point p1, p2;
+    
+    Segment(const Point& p1, const Point& p2) : p1(p1), p2(p2) {}
+};
+
+Line perpendicularBisector(const Point& a, const Point& b) {
+    Point mid = Point((a.x + b.x) / 2, (a.y + b.y) / 2);
+    Point dir = Point(a.y - b.y, b.x - a.x); 
+    return Line(mid, dir);
+}
